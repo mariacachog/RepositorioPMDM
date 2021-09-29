a2cc05a5f40be093c0b4419e552045dbdeba7d6d
María Cacho <mariacachog@hotmail.com>
2021-09-29 10:15:19 +0200

    v0.2

--- PMDM_P01Calculadora/src/CalculadoraApplet.java
+++ PMDM_P01Calculadora/src/CalculadoraApplet.java
@@ -1,5 +1,14 @@
 import javax.swing.JApplet;
+import java.awt.BorderLayout;
 
 public class CalculadoraApplet extends JApplet {
-	private InterfazCalculadora ventanaCalculadora;
+	
+	//Atributos: creamos la interfaz gráfica de la calculadora
+	private InterfazCalculadora ventanaCalculadora = new InterfazCalculadora();
+	
+	//Arranque del Applet
+	public void init() {
+		this.setLayout(new BorderLayout());
+		add(ventanaCalculadora, BorderLayout.CENTER);
+	}
 }
--- PMDM_P01Calculadora/src/InterfazCalculadora.java
+++ PMDM_P01Calculadora/src/InterfazCalculadora.java
@@ -1,14 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class InterfazCalculadora {
+public class InterfazCalculadora extends JPanel{
 	
 	private JPanel panelBotones;
 	
+	//Método para añadir botones al grid
 	private void addBoton(String texto) {
 		JButton boton = new JButton(texto);
 		panelBotones.add(boton);
 	}
 	
+	//Método para crear la interfaz de la calculadora: crea la rejilla para colocar los elementos y los inserta
 	public InterfazCalculadora() {
 		panelBotones = new JPanel(new GridLayout(4,4));
 		addBoton("7");
@@ -27,5 +30,12 @@ public class InterfazCalculadora {
 		addBoton(".");
 		addBoton("/");
 		addBoton("=");
+		
+		this.setLayout(new BorderLayout());
+		this.add(panelBotones,BorderLayout.CENTER);
+	}
+	
+	class Controlador {
+		
 	}
 }
